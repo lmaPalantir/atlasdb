f73a1a08b9d2738eb2bd4903b6e9cdf08ab74dc7
Chris Lockfort <clockfort@palantir.com>
2016-11-29 14:23:40 -0800

    remove unused executor service

--- atlasdb-cassandra-integration-tests/src/test/java/com/palantir/atlasdb/keyvalue/cassandra/CassandraKeyValueServiceIntegrationTest.java
+++ atlasdb-cassandra-integration-tests/src/test/java/com/palantir/atlasdb/keyvalue/cassandra/CassandraKeyValueServiceIntegrationTest.java
@@ -33,15 +33,12 @@ import static org.mockito.Mockito.verify;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
 import org.apache.cassandra.thrift.CfDef;
 import org.apache.cassandra.thrift.Column;
 import org.apache.cassandra.thrift.CqlRow;
 import org.apache.thrift.TException;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Ignore;
@@ -74,7 +71,6 @@ public class CassandraKeyValueServiceIntegrationTest extends AbstractKeyValueSer
             .with(new CassandraContainer());
 
     private KeyValueService keyValueService;
-    private ExecutorService executorService;
     private Logger logger = mock(Logger.class);
 
     private TableReference testTable = TableReference.createFromFullyQualifiedName("ns.never_seen");
@@ -97,12 +93,6 @@ public class CassandraKeyValueServiceIntegrationTest extends AbstractKeyValueSer
     @Before
     public void setupKvs() {
         keyValueService = getKeyValueService();
-        executorService = Executors.newFixedThreadPool(4);
-    }
-
-    @After
-    public void cleanUp() {
-        executorService.shutdown();
     }
 
     @Override
